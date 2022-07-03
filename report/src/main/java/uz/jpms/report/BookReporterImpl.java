05a262b3fe1f89761a725c99a9598f308ef71692
Ismoil <ismoil1428@gmail.com>
2022-07-03 23:19:50 +0500

    book reporter implemented

--- report/src/main/java/uz/jpms/report/BookReporterImpl.java
+++ report/src/main/java/uz/jpms/report/BookReporterImpl.java
@@ -1,28 +1,44 @@
 package uz.jpms.report;
 
 import uz.jpms.core.domain.book.Book;
+import uz.jpms.core.domain.book.BookGenre;
+import uz.jpms.core.service.BookService;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class BookReporterImpl implements BookReporter {
+
+    private final BookService service;
+
+    public BookReporterImpl(BookService service) {
+        this.service = service;
+    }
+
     @Override
     public Map<String, Integer> booksByGenre() {
-        return null;
+        Map<BookGenre, List<Book>> collect = service.findAll().stream().collect(Collectors.groupingBy(Book::getGenre));
+        return collect.entrySet()
+                .stream()
+                .collect(Collectors.toMap(key -> key.getKey().getName(), val -> val.getValue().size()));
     }
 
     @Override
     public Map<String, Integer> booksByAuthor() {
-        return null;
+        Map<String, List<Book>> collect = service.findAll().stream().collect(Collectors.groupingBy(Book::getAuthor));
+        return collect.entrySet()
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, val -> val.getValue().size()));
     }
 
     @Override
     public List<Book> availableBooks() {
-        return null;
+        return service.findAll().stream().filter(book -> book.getQuantity() > 0).collect(Collectors.toList());
     }
 
     @Override
     public List<Book> notAvailableBooks() {
-        return null;
+        return service.findAll().stream().filter(book -> book.getQuantity() <= 0).collect(Collectors.toList());
     }
 }
